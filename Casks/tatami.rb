3a212f56848b2e10751d65b9ff6541ed7cc68a5f
Kwangmin Bae / Shirou <a3457709@icloud.com>
2026-05-27 22:06:12 +0900

    Add tatami cask

---
+++ Casks/tatami.rb
@@ -0,0 +1,20 @@
+cask "tatami" do
+  version "0.1.0"
+  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+
+  url "https://github.com/PangMo5/Tatami/releases/download/v#{version}/Tatami-#{version}.dmg"
+  name "Tatami"
+  desc "macOS workspace manager with yabai-style window tiling"
+  homepage "https://github.com/PangMo5/Tatami"
+
+  auto_updates true
+  depends_on macos: ">= :sonoma"
+
+  app "Tatami.app"
+
+  zap trash: [
+    "~/.config/tatami",
+    "~/Library/Caches/dev.PangMo5.Tatami",
+    "~/Library/Preferences/dev.PangMo5.Tatami.plist",
+  ]
+end
